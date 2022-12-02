e1ff05acb3ca1fa8a2e62751a5c626b9ecca4671
Abhay Bhardwaj <86096489+iamabhay17@users.noreply.github.com>
2022-12-02 15:08:19 +0530

    Update TopArtists.jsx

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -6,10 +6,6 @@ import { useGetTopChartsQuery } from "../redux/features/shazamCore";
 const TopArtists = () => {
   const { data, isFetching, error } = useGetTopChartsQuery();
 
-  if (isFetching) return <Loader title="Loading artists..." />;
-
-  if (error) return <Error />;
-
   return (
     <div className="flex flex-col">
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
