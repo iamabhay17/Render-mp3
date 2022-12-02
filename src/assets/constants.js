1bfd012869dbf6c12a14d0b4f1e1c8944eeddf8b
Abhay Bhardwaj <86096489+iamabhay17@users.noreply.github.com>
2022-12-02 15:43:59 +0530

    Update constants.js

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -24,6 +24,5 @@ export const genres = [
 
 export const links = [
   { name: "Discover", to: "/", icon: HiOutlineHome },
-  { name: "Top Artists", to: "/top-artists", icon: HiOutlineUserGroup },
-  { name: "Top Charts", to: "/top-charts", icon: HiOutlineHashtag },
+  
 ];
